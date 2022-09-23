83c62434481f69124b629324a3e026a125c7100a
fais edathil <faiesedathil@gmail.com>
2022-09-23 15:04:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+      Framer Motion Tutorial For Beginners
+    </h1>
+    <h3>Each chapter is saved as seperate branches.
+    </h3>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
