da2bdf705f30a7746223e936509328e99bb3234b
fais edathil <faiesedathil@gmail.com>
2022-09-23 17:10:36 +0300

    when property

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import './App.css'
-import { motion } from 'framer-motion/dist/framer-motion'
+        import React from 'react'
+        import './App.css'
+        import { motion } from 'framer-motion/dist/framer-motion'
+        function App() {
+          const myVariants = {
+            start: { x:1000 },
+            final: { x:0, transition: { type:'tween'} ,
+                    transition:{when:'beforeChildren'}}
+          }
+          const childVariants = {
+            start: {opacity: 0},
+            final: { opacity: 1,
+                    transition:{ duration:2}}
+          }
+              return (
+                <motion.div
+                    variants={myVariants}
+                    initial='start'
+                    animate='final'
+                >
+                  <h3>Hello World</h3>
+                 <div>
+                    <motion.p variants={childVariants} >
+                       This is a sample text
+                     </motion.p>
+                </div>
+               </motion.div>
+          )
+        }
 
-function App() {
-  const myVariants={
-    start:{
-      opacity:0
-    },
-    final:{
-      opacity:1,
-      transition:{
-        duration:2
-      }
-    }
-  }
-  return (
-    <div>
-      <motion.h1
-    variants={myVariants}
-    initial='start'
-    animate='final'
-      >
-        Hello World
-      </motion.h1>
-    </div>
-  )
-}
-
-export default App
+        export default App
