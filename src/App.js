68cc39c312047670c66d5e298a93f17aee2364c4
fais edathil <faiesedathil@gmail.com>
2022-09-23 15:25:41 +0300

    transition

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import './App.css'
-import {motion} from 'framer-motion/dist/framer-motion'
+import { motion } from 'framer-motion/dist/framer-motion'
 
 function App() {
   return (
     <div>
       <motion.h1
-      initial={{x:300}}
-      animate={{x:0}}
+        initial={{ x: 300 , opacity:0}}
+        animate={{ x: 0, opacity:1 }}
+        transition={{
+          type:'spring',
+          stiffness:120
+        }}
       >
-    Hello World
-    </motion.h1>
- </div>
+        Hello World
+      </motion.h1>
+    </div>
   )
 }
 
