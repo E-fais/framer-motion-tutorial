bc16ec7516be7414f626178618ed6440a6b6610e
fais edathil <faiesedathil@gmail.com>
2022-09-23 15:40:04 +0300

    variants

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css'
 import { motion } from 'framer-motion/dist/framer-motion'
 
 function App() {
+  const myVariants={
+    start:{
+      opacity:0
+    },
+    final:{
+      opacity:1,
+      transition:{
+        duration:2
+      }
+    }
+  }
   return (
     <div>
-      <motion.button
-       whileHover={{
-        scale:1.1,
-        background:'red'
-      }}
+      <motion.h1
+    variants={myVariants}
+    initial='start'
+    animate='final'
       >
         Hello World
-      </motion.button>
+      </motion.h1>
     </div>
   )
 }
