b33e149c8785a3d4eb36629ee3fa37a430029d97
fais edathil <faiesedathil@gmail.com>
2022-09-24 22:50:14 +0300

    change animtion type to string

--- src/App.js
+++ src/App.js
@@ -4,40 +4,30 @@
         function App() {
           const myVariants = {
             start: { x:1000 },
-            final: { x:0, transition: { type:'tween'} ,
-                    transition:
-                    {when:'beforeChildren',
-                    staggerChildren:0.4,
-                    damping:8,
-                    mass:0.4
-                  }}
-          }
+            final: { x:0, transition: 
+                    {type:'spring',  when:'beforeChildren', staggerChildren:0.4, damping:8, mass:0.4 }
+                  }
+                }
           const childVariants = {
             start: {x: '-120vw'},
             final: { x: 0,
-                     transition:{
-                      damping:0.8,
-                      mass:5
-                    }
+                     transition:{ type:'tween' }
             }}
               return (
                 <motion.div
-                    variants={myVariants}
-                    initial='start'
-                    animate='final'
-                >
+                  variants={myVariants}
+                  initial='start'
+                  animate='final' >
                   <h3>Hello World</h3>
-                 <div>
+                  <div>
                     <motion.p variants={childVariants} >
-                       This is a sample text
-                     </motion.p>
+                      This is a sample text
+                    </motion.p>
                     <motion.p variants={childVariants} >
-                       This is another text
-                     </motion.p>
-                     
-                </div>
-               </motion.div>
+                      This is another text
+                    </motion.p>
+                  </div>
+                </motion.div>
           )
         }
-
         export default App
