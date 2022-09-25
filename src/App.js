fc9717fefafd865335fb73ed10ac40da2c6392ca
fais edathil <faiesedathil@gmail.com>
2022-09-25 20:10:30 +0300

    add yoyo section

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-        import React from 'react'
-        import './App.css'
-        import { motion } from 'framer-motion/dist/framer-motion'
-        function App() {
-          const myVariants = {
-            start: { x:1000 },
-            final: { x:0, transition: 
-                    {type:'spring',  when:'beforeChildren', staggerChildren:0.4, damping:8, mass:0.4 }
-                  }
-                }
-          const childVariants = {
-            start: {x: '-120vw'},
-            final: { x: 0,
-                     transition:{ type:'tween' }
-            }}
-              return (
-                <motion.div
-                  variants={myVariants}
-                  initial='start'
-                  animate='final' >
-                  <h3>Hello World</h3>
-                  <div>
-                    <motion.p variants={childVariants} >
-                      This is a sample text
-                    </motion.p>
-                    <motion.p variants={childVariants} >
-                      This is another text
-                    </motion.p>
-                  </div>
-                </motion.div>
-          )
-        }
-        export default App
+import React from 'react'
+import './App.css'
+import { motion } from 'framer-motion/dist/framer-motion'
+function App() {
+
+  const myVariants = {
+      start:{
+    rotateY:120
+      },
+        final:{
+          rotateY:0,
+          transition:{
+            yoyo:Infinity,
+            duration:3
+          }}
+  }
+      return (
+        <div>
+          <motion.h1
+          variants={myVariants}
+          initial='start'
+          animate='final'>
+            YoYo keyframe  
+          </motion.h1>
+        </div>
+      )
+}
+export default App
