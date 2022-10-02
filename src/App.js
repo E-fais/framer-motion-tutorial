777e072cd24355a648134dc9f224bb4b3028137f
fais edathil <faiesedathil@gmail.com>
2022-10-02 20:29:12 +0300

    use cycle

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import { motion } from 'framer-motion/dist/framer-motion'
+import { motion,useCycle } from 'framer-motion/dist/framer-motion'
 import React from 'react'
 import './App.css'
-
 function App() {
-  const loaderVariant={
-    animationOne:{
-      x:[-20,20],
-      y:[0,-30]
-    ,
-  transition:{
-    x:{
-      yoyo:Infinity,
-      duration:0.5
-    },
-    y:{
-      yoyo:Infinity,
-      duration:0.25,
-      ease: 'easeOut'
+  const [animation,cycleAnimation]=useCycle("animationOne","animationTwo")
+  const loaderVariant = {
+    animationOne: { x: [-20, 20],
+                    y: [0, -30],
+                   transition: { x: { yoyo: Infinity, duration: 0.5 },
+                                 y: {yoyo: Infinity,duration: 0.25,ease: 'easeOut' }}},
+    animationTwo:{
+         X:0,
+          y:[0,-40],transition:{
+          y:{ yoyo:Infinity, duration:0.25, ease:'easeOut'}
+      }
     }
   }
-  }}
- 
   return (
     <div>
-       <motion.div
-      class='loader'
-      variants={loaderVariant}
-      animate='animationOne'
-      >
+      <motion.div
+        class='loader'
+        variants={loaderVariant}
+        animate={animation} >
       </motion.div>
-      <p>Loading</p>
-     
+      <p onClick={()=>cycleAnimation()}>Cycle Animation</p>
     </div>
   )
 }
